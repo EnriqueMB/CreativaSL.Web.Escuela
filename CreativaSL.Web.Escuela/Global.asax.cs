cad867c53ff3a82fa05131dd8c9ed3874369d5fa
EnriqueMB <kikeballina1@gmail.com>
2018-01-09 17:28:47 -0600

    Cambios

--- CreativaSL.Web.Escuela/Global.asax.cs
+++ CreativaSL.Web.Escuela/Global.asax.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace CreativaSL.Web.Escuela
@@ -12,7 +14,12 @@ namespace CreativaSL.Web.Escuela
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
         }
     }
 }
