6cc5b1ed593ceaa7b58971a4d07bb7324301720d
EnriqueMB <kikeballina1@gmail.com>
2018-01-11 09:08:07 -0600

    CatModadalidad

--- CreativaSL.Web.Escuela/Areas/Admin/Controllers/CatModalidadController.cs
+++ CreativaSL.Web.Escuela/Areas/Admin/Controllers/CatModalidadController.cs
@@ -35,71 +35,165 @@ namespace CreativaSL.Web.Escuela.Areas.Admin.Controllers
             }
         }
 
-        // GET: Admin/CatModalidad/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
         // GET: Admin/CatModalidad/Create
+        [HttpGet]
+        [Authorize(Roles = "4")]
         public ActionResult Create()
         {
-            return View();
+            try
+            {
+                CatModalidadModels Modalidad = new CatModalidadModels();
+                CatModalidad_Datos ModalidadDatos = new CatModalidad_Datos();
+                Modalidad.conexion = Conexion;
+                Modalidad.TablaPlanEstudioCmb = ModalidadDatos.obtenerComboCatPlanEstudio(Modalidad);
+                var list = new SelectList(Modalidad.TablaPlanEstudioCmb, "IDPlanEstudio", "Descripcion");
+                ViewData["cmbPlanEstudio"] = list;
+                return View(Modalidad);
+            }
+            catch (Exception)
+            {
+                CatModalidadModels Modalidad = new CatModalidadModels();
+                TempData["typemessage"] = "2";
+                TempData["message"] = "No se puede cargar la vista";
+                return View(Modalidad);
+            }
+           
         }
 
         // POST: Admin/CatModalidad/Create
         [HttpPost]
+        [Authorize(Roles ="4")]
         public ActionResult Create(FormCollection collection)
         {
             try
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
+                CatModalidadModels Modalidad = new CatModalidadModels();
+                CatModalidad_Datos ModalidadDatos = new CatModalidad_Datos();
+                Modalidad.conexion = Conexion;
+                Modalidad.opcion = 1;
+                Modalidad.user = User.Identity.Name;
+                Modalidad.IDModalidad = "";
+                Modalidad.IDPlanEstudio = Convert.ToInt32(collection["TablaPlanEstudioCmb"]);
+                Modalidad.Descripcion = collection["Descripcion"];
+                Modalidad = ModalidadDatos.AbcCatModalidad(Modalidad);
+                if (Modalidad.Completado == true)
+                {
+                    TempData["typemessage"] = "1";
+                    TempData["message"] = "Los datos se guardaron correctamente.";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    Modalidad.TablaPlanEstudioCmb = ModalidadDatos.obtenerComboCatPlanEstudio(Modalidad);
+                    var list = new SelectList(Modalidad.TablaPlanEstudioCmb, "IDPlanEstudio", "Descripcion");
+                    ViewData["cmbPlanEstudio"] = list;
+                    TempData["typemessage"] = "2";
+                    TempData["message"] = "Los datos no se guardaron correctamente. Intente nuevamente";
+                    return View(Modalidad);
+                }
             }
             catch
             {
-                return View();
+                TempData["typemessage"] = "2";
+                TempData["message"] = "Los datos no se guardaron correctamente. Contacte a soporte técnico.";
+                return RedirectToAction("Index");
             }
         }
 
         // GET: Admin/CatModalidad/Edit/5
-        public ActionResult Edit(int id)
+        [HttpGet]
+        [Authorize(Roles = "4")]
+        public ActionResult Edit(string id)
         {
-            return View();
+            try
+            {
+                CatModalidadModels Modalidad = new CatModalidadModels();
+                CatModalidad_Datos ModalidadDatos = new CatModalidad_Datos();
+                Modalidad.conexion = Conexion;
+                Modalidad.IDModalidad = id;
+                Modalidad.TablaPlanEstudioCmb = ModalidadDatos.obtenerComboCatPlanEstudio(Modalidad);
+                var list = new SelectList(Modalidad.TablaPlanEstudioCmb, "IDPlanEstudio", "Descripcion");
+                ViewData["cmbPlanEstudio"] = list;
+                ModalidadDatos.ObtenerDetalleCatModalidad(Modalidad);
+                return View(Modalidad);
+            }
+            catch (Exception)
+            {
+                CatModalidadModels Modalidad = new CatModalidadModels();
+                TempData["typemessage"] = "2";
+                TempData["message"] = "No se puede cargar la vista";
+                return RedirectToAction("Index");
+            }
         }
 
         // POST: Admin/CatModalidad/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        [Authorize( Roles = "4")]
+        public ActionResult Edit(string id, FormCollection collection)
         {
             try
             {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
+                CatModalidadModels Modalidad = new CatModalidadModels();
+                CatModalidad_Datos ModalidadDatos = new CatModalidad_Datos();
+                Modalidad.conexion = Conexion;
+                Modalidad.opcion = 2;
+                Modalidad.user = User.Identity.Name;
+                Modalidad.IDModalidad = collection["IDModalidad"];
+                Modalidad.IDPlanEstudio = Convert.ToInt32(collection["TablaPlanEstudioCmb"]);
+                Modalidad.Descripcion = collection["Descripcion"];
+                Modalidad = ModalidadDatos.AbcCatModalidad(Modalidad);
+                if (Modalidad.Completado == true)
+                {
+                    TempData["typemessage"] = "1";
+                    TempData["message"] = "Los datos se editarón correctamente.";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["typemessage"] = "2";
+                    TempData["message"] = "Los datos no se editarón correctamente. Intente nuevamente";
+                    return View(Modalidad);
+                }
             }
             catch
             {
-                return View();
+                TempData["typemessage"] = "2";
+                TempData["message"] = "Los datos no se guardaron correctamente. Contacte a soporte técnico.";
+                return RedirectToAction("Index");
             }
         }
 
         // GET: Admin/CatModalidad/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(string id)
         {
             return View();
         }
 
         // POST: Admin/CatModalidad/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(string id, FormCollection collection)
         {
             try
             {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
+                CatModalidadModels Modalidad = new CatModalidadModels();
+                CatModalidad_Datos ModalidadDatos = new CatModalidad_Datos();
+                Modalidad.conexion = Conexion;
+                Modalidad.IDModalidad = id;
+                Modalidad.opcion = 3;
+                Modalidad.user = User.Identity.Name;
+                Modalidad = ModalidadDatos.AbcCatModalidad(Modalidad);
+                if (Modalidad.Completado == true)
+                {
+                    TempData["typemessage"] = "1";
+                    TempData["message"] = "El registro se elimino correctamente.";
+                    return Json("");
+                }
+                else
+                {
+                    TempData["typemessage"] = "2";
+                    TempData["message"] = "Ocurrio un error al eliminar. Intente nuevamente.";
+                    return Json("");
+                }
             }
             catch
             {
