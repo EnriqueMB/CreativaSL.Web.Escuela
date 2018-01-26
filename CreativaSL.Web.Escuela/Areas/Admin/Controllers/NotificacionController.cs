180347eb1e3a9f238dec650dfaa228d36c884418
Eder Ramírez <eder-ramtz@hotmail.com>
2018-01-26 13:39:57 -0600

    mm

---
+++ CreativaSL.Web.Escuela/Areas/Admin/Controllers/NotificacionController.cs
@@ -0,0 +1,95 @@
+﻿using CreativaSL.Web.Escuela.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreativaSL.Web.Escuela.Areas.Admin.Controllers
+{
+    public class NotificacionController : Controller
+    {
+        string Conexion = ConfigurationManager.AppSettings.Get("strConnection");
+        // GET: Admin/Notificacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        //[Authorize(Roles = "3")]
+        public ActionResult Create()
+        {
+            try
+            {
+                NotificacionModels Notificacion = new NotificacionModels();
+
+                Notificacion.conexion = Conexion;
+                
+                return View(Notificacion);
+            }
+            catch (Exception)
+            {
+                NotificacionModels Notificacion = new NotificacionModels();
+                TempData["typemessage"] = "2";
+                TempData["message"] = "No se puede cargar la vista";
+                return View(Notificacion);
+            }
+
+        }
+        [HttpPost]
+        //[Authorize(Roles = "3")]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                NotificacionModels Notificacion = new NotificacionModels();
+                _Notificacion_Datos NotificacionDatos = new _Notificacion_Datos();
+                Notificacion.conexion = Conexion;
+                Notificacion.IDNotificacion = "";
+                Notificacion.nombre = collection["nombre"];
+                Notificacion.Materia = collection["materia"];
+                Notificacion.fecha = DateTime.ParseExact(collection["fecha"], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                Notificacion.cadena = collection["cadena"]; 
+                Notificacion = NotificacionDatos.CadenaFinal(Notificacion);
+                Notificacion.user = User.Identity.Name;
+                Notificacion.opcion = 1;
+                //Notificacion = CatedraticoDatos.AbcCatCatedratico(Catedratico);
+                //if (string.IsNullOrEmpty(Catedratico.id_persona))
+                //{
+
+                //    TempData["typemessage"] = "2";
+                //    TempData["message"] = "El usuario ingresado ya existe.";
+                //    return RedirectToAction("Create");
+                //}
+                //else
+                //{
+                //    Comun.EnviarCorreo(
+                //    ConfigurationManager.AppSettings.Get("CorreoTxt")
+                //   , ConfigurationManager.AppSettings.Get("PasswordTxt")
+                //   , Catedratico.correo
+                //   , "Registro Profesor"
+                //   , Comun.GenerarHtmlRegistoUsuario(Catedratico.clvUser, Catedratico.passUser)
+                //   , false
+                //   , ""
+                //   , Convert.ToBoolean(ConfigurationManager.AppSettings.Get("HtmlTxt"))
+                //   , ConfigurationManager.AppSettings.Get("HostTxt")
+                //   , Convert.ToInt32(ConfigurationManager.AppSettings.Get("PortTxt"))
+                //   , Convert.ToBoolean(ConfigurationManager.AppSettings.Get("EnableSslTxt")));
+                //    TempData["typemessage"] = "1";
+                //    TempData["message"] = "Los datos se guardaron correctamente.";
+                //    return RedirectToAction("Index");
+                //}
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+
+                TempData["typemessage"] = "2";
+                TempData["message"] = "Ocurrio un error el intentar guardar. Contacte a soporte técnico" + ex;
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ CreativaSL.Web.Escuela/Models/NotificacionModels.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace CreativaSL.Web.Escuela.Models
+{
+    public class NotificacionModels
+    {
+        private string _IDNotificacion;
+
+        public string IDNotificacion
+        {
+            get { return _IDNotificacion; }
+            set { _IDNotificacion = value; }
+        }
+
+        private string _cadena;
+        [Required(ErrorMessage = "La descripción es obligatoria")]
+        [Display(Name = "descripción")]
+        [StringLength(350, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2} y un maximo {1}.", MinimumLength = 1)]
+        [RegularExpression(@"^[A-Za-záéíóúñÁÉÍÓÚÑ0-9\(\)\[\]\-\,\.\;\:\s]*$", ErrorMessage = "Solo Letras y números")]
+        public string cadena
+        {
+            get { return _cadena; }
+            set { _cadena = value; }
+        }
+        private string _materia;
+
+        public string Materia
+        {
+            get { return _materia; }
+            set { _materia = value; }
+        }
+
+        private List<ParejaPalabra> _listListValues;
+
+        public List<ParejaPalabra> ListValues
+        {
+            get { return _listListValues; }
+            set { _listListValues = value; }
+        }
+
+
+        private DataTable _TablaDatos;
+
+        public DataTable TablaDatos
+        {
+            get { return _TablaDatos; }
+            set { _TablaDatos = value; }
+        }
+        private string _textoFinal;
+
+        public string textoFinal
+        {
+            get { return _textoFinal; }
+            set { _textoFinal = value; }
+        }
+        private DateTime _fecha = DateTime.Now ;
+
+        public DateTime fecha
+        {
+            get { return _fecha; }
+            set { _fecha = value; }
+        }
+
+        private string _nombre;
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [Display(Name = "nombre")]
+        [StringLength(100, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2} y un maximo {1}.", MinimumLength = 1)]
+        [RegularExpression(@"^[A-Za-záéíóúñÁÉÍÓÚÑ\-\s]*$", ErrorMessage = "Solo Letras y guión")]
+        public string nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        #region Datos de control
+        public int Resultado { get; set; }
+        public bool Completado { get; set; }
+        public bool activo { get; set; }
+        public string user { get; set; }
+        public string conexion { get; set; }
+        public int opcion { get; set; }
+        #endregion
+    }
+}
