b0255fc967bc35a3a5869094d754b3d608221036
EnriqueMB <kikeballina1@gmail.com>
2018-02-02 15:17:37 -0600

    administradorProfesor

---
+++ CreativaSL.Web.Escuela/Areas/Profesor/Controllers/CatCuentaUserController.cs
@@ -0,0 +1,70 @@
+﻿using CreativaSL.Web.Escuela.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreativaSL.Web.Escuela.Areas.Profesor.Controllers
+{
+    public class CatCuentaUserController : Controller
+    {
+        string Conexion = ConfigurationManager.AppSettings.Get("strConnection");
+        // GET: Profesor/CatCuentaUser
+        public ActionResult Index()
+        {
+            try
+            {
+                CatCuentaUserModels cuentaUser = new CatCuentaUserModels();
+                CatCuentaUser_Datos cuentaUser_datos = new CatCuentaUser_Datos();
+                cuentaUser.conexion = Conexion;
+                cuentaUser.id_cuenta = User.Identity.Name;
+                return View(cuentaUser);
+            }
+            catch (Exception)
+            {
+                CatCuentaUserModels cuentaUser = new CatCuentaUserModels();
+                cuentaUser.tablaUsuarioCuenta = new DataTable();
+                TempData["typemessage"] = "2";
+                TempData["message"] = "No se puede cargar la vista";
+                return View(cuentaUser);
+            }
+        }
+
+        public ActionResult Index(FormCollection collection)
+        {
+            try
+            {
+                CatCuentaUserModels cuentaUser = new CatCuentaUserModels();
+                CatCuentaUser_Datos cuentaUser_datos = new CatCuentaUser_Datos();
+                cuentaUser.conexion = Conexion;
+                cuentaUser.id_cuenta = User.Identity.Name;
+                cuentaUser.passUser = collection["passUser"];
+                cuentaUser.passActualizado = collection["passActualizado"];
+                cuentaUser.user = User.Identity.Name;
+                cuentaUser = cuentaUser_datos.AbcCambiarContraseña(cuentaUser);
+                if (cuentaUser.id_cuenta == Convert.ToString(1))
+                {
+                    TempData["typemessage"] = "1";
+                    TempData["message"] = "La Contraseña sea creado correctamente";
+                }
+                else
+                {
+                    TempData["typemessage"] = "2";
+                    TempData["message"] = "La Contraseña no Coincide";
+                }
+                return View(cuentaUser);
+            }
+            catch (Exception)
+            {
+                CatCuentaUserModels cuentaUser = new CatCuentaUserModels();
+                cuentaUser.tablaUsuarioCuenta = new DataTable();
+                TempData["typemessage"] = "2";
+                TempData["message"] = "La Contraseña no Coinciden";
+                return View(cuentaUser);
+            }
+        }
+    }
+}
